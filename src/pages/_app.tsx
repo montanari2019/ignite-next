bd4adb0a730005a439ebfc278620abc1bd50455c
@ikaromontanarib3 <desenvolvimento4@grupoib3.com.br>
2023-04-13 16:02:25 -0400

    prosseguindo no nextJS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+import logoIgnite from "../assets/logo.svg"
+import { Container, Header } from '../styles/stylesComponents/StyledApp'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoIgnite.src} alt=''/>
+      </Header>
+      <Component {...pageProps} />
+
+    </Container>
+  )
 }
---
+++ src/styles/DefaultStyles.ts
@@ -0,0 +1,15 @@
+import { createStitches } from "@stitches/react";
+
+export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme, config} = createStitches({
+    theme:{
+        colors:{
+            green500:"#00875F",
+            green300:"#00B37E",
+            gray300:"#C4C4CC",
+            gray100:"#E1E1E6",
+            gray800:"#202024",
+            gray900:"#121214",
+        }
+    }
+    
+})
